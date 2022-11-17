695ffe9d6dd6872baa5f12967513dc38fc731631
JacobJB08 <721378@student.dusd.net>
2022-11-17 09:53:34 -0800

    Added health/healing

--- RubysAdventureBonilla/Assets/Scripts/RubyController.cs
+++ RubysAdventureBonilla/Assets/Scripts/RubyController.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public float speed = 3.0f;
     public int maxHealth = 5;
+    public int health { get { return currentHealth; } }
     int currentHealth;
     Rigidbody2D rigidbody2d;
     float horizontal;
@@ -27,12 +29,12 @@ public class RubyController : MonoBehaviour
     void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
-
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
         rigidbody2d.MovePosition(position);
     }
-    void ChangeHealth(int amount)
+
+    public void ChangeHealth(int amount)
     {
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
