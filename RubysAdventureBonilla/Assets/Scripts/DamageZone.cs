06eca869046ec03bc179bcc28dc3ea88ae669cf1
JacobJB08 <721378@student.dusd.net>
2022-12-02 09:55:22 -0800

    added enemies and obstacles

--- RubysAdventureBonilla/Assets/Scripts/DamageZone.cs
+++ RubysAdventureBonilla/Assets/Scripts/DamageZone.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class DamageZone : MonoBehaviour
 {
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
     void OnTriggerStay2D(Collider2D other)
     {
         RubyController controller = other.GetComponent<RubyController>();
